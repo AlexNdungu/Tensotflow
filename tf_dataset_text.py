775dbb569027b04100f0349ec88c131ffcfcb656
AlexNdungu <alexmeta517@gmail.com>
2023-08-16 11:01:10 +0300

    tensorflow datasets minist

---
+++ tf_dataset_text.py
@@ -0,0 +1,4 @@
+import tensorflow as tf
+from tensorflow import keras
+import tensorflow_datasets as tfds
+import matplotlib.pyplot
--- tf_datasets.py
+++ tf_datasets.py
@@ -1,9 +1,6 @@
 import tensorflow as tf
 from tensorflow import keras
-#from tensorflow.keras import layers
 import tensorflow_datasets as tfds
-import matplotlib.pyplot
-
 
 (ds_train, ds_test), ds_info = tfds.load(
     "mnist",
@@ -13,4 +10,39 @@ import matplotlib.pyplot
     with_info=True
 )
 
-print(ds_info)
+def normalize_image(image, label):
+
+    #normalize images
+
+    return tf.cast(image, tf.float32)/255.0, label
+
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+BATCH_SIZE = 64
+ds_train = ds_train.map(normalize_image, num_parallel_calls=AUTOTUNE)
+ds_train = ds_train.cache()
+ds_train = ds_train.shuffle(ds_info.splits["train"].num_examples)
+ds_train = ds_train.batch(BATCH_SIZE)
+ds_train = ds_train.prefetch(AUTOTUNE)
+
+
+ds_test = ds_test.map(normalize_image, num_parallel_calls=AUTOTUNE)
+ds_test = ds_test.batch(128)
+ds_test = ds_test.prefetch(AUTOTUNE)
+
+model = keras.Sequential([
+    keras.Input((28,28, 1)),
+    keras.layers.Conv2D(32, 3, activation='relu'),
+    keras.layers.Flatten(),
+    keras.layers.Dense(10)
+])
+
+model.compile(
+    optimizer=keras.optimizers.Adam(learning_rate=0.001),
+    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=["accuracy"],
+)
+
+model.fit(ds_train, epochs=5, verbose=2)
+
+model.evaluate(ds_test)
