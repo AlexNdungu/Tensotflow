1586e9d2fd67b23824332fa7e1c33eeb2bc664e0
AlexNdungu <alexmeta517@gmail.com>
2023-08-24 18:33:10 +0300

    data augmentation

--- data_augmentation.py
+++ data_augmentation.py
@@ -10,3 +10,47 @@ import tensorflow_datasets as tfds
     as_supervised=True,
     with_info=True,
 )
+
+#Normalizing the data
+def normalize_img(image, label):
+    return tf.cast(image, tf.float32) / 255.0, label
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+BATCH_SIZE = 32
+
+# setup for train dataset
+ds_train = ds_train.map(normalize_img, num_parallel_calls=AUTOTUNE)
+ds_train = ds_train.cache()
+ds_train = ds_train.shuffle(ds_info.splits["train"].num_examples)
+ds_train = ds_train.batch(BATCH_SIZE)
+ds_train = ds_train.prefetch(AUTOTUNE)
+
+# setup for test dataset
+ds_test = ds_test.map(normalize_img, num_parallel_calls=AUTOTUNE)
+ds_test = ds_test.batch(BATCH_SIZE)
+ds_test = ds_test.prefetch(AUTOTUNE)
+
+
+# Build the model
+model = keras.Sequential(
+    [
+        keras.Input(shape=(32, 32, 3)),
+        layers.Conv2D(4, 3, padding="same", activation="relu"),
+        layers.Conv2D(8, 3, padding="same", activation="relu"),
+        layers.MaxPooling2D(),
+        layers.Conv2D(16, 3, padding="same", activation="relu"),
+        layers.Flatten(),
+        layers.Dense(64, activation="relu"),
+        layers.Dense(10),
+    ]
+)
+
+# Compile the model
+model.compile(
+    optimizer=keras.optimizers.Adam(3e-4),
+    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=["accuracy"],
+)
+
+model.fit(ds_train, epochs=5, verbose=2)
+model.evaluate(ds_test)
